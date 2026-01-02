40a6ba15c0d69df099d800efedf58763d0569656
Lucky Umoka <umokalucky@gmail.com>
2026-01-02 13:03:15 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/zuckky/UserProfile.tsx
+++ src/components/zuckky/UserProfile.tsx
@@ -31,7 +31,7 @@ export function UserProfile() {
           </div>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
+      <DropdownMenuContent className="w-56" align="end">
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
             <p className="text-sm font-medium leading-none">User</p>
