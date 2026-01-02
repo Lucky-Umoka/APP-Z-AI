f7c40ccfea9661b09868c94a1c4245c14419020c
Lucky Umoka <umokalucky@gmail.com>
2026-01-02 15:09:54 +0000

    the same thing. WHY IS IT STILL AT THE LEFT. i NEED IT TO BE AT THE CENT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="flex min-h-screen">
         <ZuckkySidebar />
         <SidebarInset>
-          <main className="flex h-screen w-full flex-col">
+          <main className="flex h-screen w-full flex-col items-center justify-center">
             <ChatInterface />
           </main>
         </SidebarInset>
