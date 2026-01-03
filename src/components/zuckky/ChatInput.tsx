9d436f97a519c9a83eb37b6c39d6efe42140f17a
Lucky Umoka <umokalucky@gmail.com>
2026-01-03 02:23:12 +0000

    Great job! But there is no entrance or exit motion or animation of the b

--- src/components/zuckky/ChatInput.tsx
+++ src/components/zuckky/ChatInput.tsx
@@ -77,14 +77,14 @@ const ChatInput = forwardRef<{ setFile: (file: File) => void }, ChatInputProps>(
         <div 
         className={cn(
             'relative rounded-2xl p-px group',
-            'focus-within:bg-gradient-to-r focus-within:from-green-400 focus-within:to-blue-500'
+            file ? 'mb-2' : ''
         )}
         >
         <Card className={cn(
           'w-full rounded-[15px] bg-card transition-all relative',
           'before:absolute before:inset-0 before:rounded-[15px] before:p-px before:bg-[conic-gradient(from_var(--angle),_#00C2FF_0%,_#39FF14_30%,_#00C2FF_60%)]',
           'before:opacity-0 focus-within:before:opacity-100 before:transition-opacity before:duration-500 before:animate-border-flow',
-          file ? 'mb-2' : ''
+          'focus-within:ring-0'
         )}>
             <div className="relative bg-card rounded-[14px]">
                 {file && (
