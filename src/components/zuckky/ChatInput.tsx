56df411106e9513b171987a3ac47485927b6d7a9
Lucky Umoka <umokalucky@gmail.com>
2026-01-03 02:56:52 +0000

    Reduce the thickness of the border. Ensure it is a think line and reduce

--- src/components/zuckky/ChatInput.tsx
+++ src/components/zuckky/ChatInput.tsx
@@ -75,8 +75,8 @@ const ChatInput = forwardRef<{ setFile: (file: File) => void }, ChatInputProps>(
 
   return (
     <div className="w-full">
-        <div className={cn('relative w-full rounded-2xl bg-card border-2 border-border transition-all duration-500 focus-within:border-transparent focus-within:p-[2px] focus-within:bg-gradient-to-r focus-within:from-[#00ff87] focus-within:via-[#60efff] focus-within:to-[#0099ff] focus-within:animate-border-flow', file ? 'mb-2' : '')}>
-          <div className="relative w-full rounded-[14px] bg-card">
+        <div className={cn('relative w-full rounded-2xl bg-card border-2 border-border transition-all duration-700 focus-within:border-transparent focus-within:p-px focus-within:bg-gradient-to-r focus-within:from-[#00ff87] focus-within:via-[#60efff] focus-within:to-[#0099ff] focus-within:animate-border-flow', file ? 'mb-2' : '')}>
+          <div className="relative w-full rounded-xl bg-card">
             {file && (
             <div className="p-3 border-b border-border">
                 <div className="relative flex items-center gap-3 p-2 rounded-lg bg-background w-fit">
