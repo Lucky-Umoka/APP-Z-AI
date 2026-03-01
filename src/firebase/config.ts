7f6046301f6f03a4ccb7abea93fc4f649f78fc16
Lucky Umoka <umokalucky@gmail.com>
2026-03-01 10:26:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "zuckky-ai-frontend-11414-94786",
+  "appId": "1:536960589417:web:de7b2ddae1fffd5e47ccf8",
+  "apiKey": "AIzaSyChnERrj4SYmwYa2rqkE3CqF7YJC78plI0",
+  "authDomain": "zuckky-ai-frontend-11414-94786.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "536960589417"
+};
